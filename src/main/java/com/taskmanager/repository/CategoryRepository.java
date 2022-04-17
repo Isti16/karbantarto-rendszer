f12088c03190a4f8f336c1342b4bdae133862f4c
Isti16 <isti001116@gmail.com>
2022-04-17 23:31:03 +0200

    edited

--- src/main/java/com/taskmanager/repository/CategoryRepository.java
+++ src/main/java/com/taskmanager/repository/CategoryRepository.java
@@ -1,5 +1,13 @@
 package com.taskmanager.repository;
 
-public class CategoryRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.taskmanager.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategory(String user);
 }
+
